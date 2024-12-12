ee7e012fb818f925195e30f1eece4eee0aefb18b
rajanunagar <86923104+rajanunagar@users.noreply.github.com>
2024-12-12 23:48:20 +0530

    create book lambda added

---
+++ resolvers/lambda/createBook.js
@@ -0,0 +1,36 @@
+"use strict";
+const { v4 } = require("uuid");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient,PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { BOOKS_TABLE } = process.env;
+const dynamoDBClient = new DynamoDBClient({
+  region: 'us-west-1',
+});
+const documentClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+module.exports.handler = async (event) => {
+  if (event.field === "createBook") {
+    var { title,author,price } = event.arguments;
+    try {
+    const id = v4();
+    const newItem = {
+        bookId: id,
+        title,
+        author,
+        price,
+        createdAt: new Date().toISOString(),
+      };
+    const params = {
+      TableName: BOOKS_TABLE,
+      Item:newItem
+    };
+    const result = await documentClient.send(new PutCommand(params));
+    return newItem;
+  } catch (err) {
+    return err;
+  }
+}
+else{
+    return null;
+}
+};
